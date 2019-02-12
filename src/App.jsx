f624f776146e3068ff8ef781172ef1a713769176
silentscribe <seelive.i@gmail.com>
2019-02-12 21:41:53 +0000

    messages being added by user successfully updating state and rerendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Chatbar from './Chatbar.jsx';
 import MessageFeed from './MessageList.jsx';
-import messages from './MessageDB.json'
 
 
 class Navbar extends Component {
@@ -14,32 +13,62 @@ class Navbar extends Component {
   }
 }
 
+const generateRandomId = (alphabet => {
+  const alphabetLength = alphabet.length;
+  const randoIter = (key, n) => {
+    if (n === 0) {
+      return key;
+    }
+    const randoIndex = Math.floor(Math.random() * alphabetLength);
+    const randoLetter = alphabet[randoIndex];
+    return randoIter(key + randoLetter, n - 1);
+  };
+  return () => randoIter("", 5);
+})("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
       loading: true,
-      messages: {
-        currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-        messages: [
-          {
-            id: 234544,
-            username: "Bob",
-            content: "Has anyone seen my marbles?",
-          },
-          {
-            id: 456788,
-            username: "Anonymous",
-            content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-          }
-        ]
-      }
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 234544,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 456788,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
     };
+    this.addNewMessage = this.addNewMessage.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ loading: false });
+    console.log("componentDidMount <App />");
+    this.setState({ loading: false })
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = [...this.state.messages, newMessage];
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
   }
+
+  addNewMessage(user, message) {
+    const newMessage = { id: generateRandomId(), username: user, content: message };
+    const messages = [...this.state.messages, newMessage];
+    this.setState({messages: messages});
+  }
+
   render() {
     if (this.state.loading) {
       return <h1>Page Loading...</h1>
@@ -48,9 +77,9 @@ class App extends Component {
         <div>
         <Navbar />
         <main className="messages">
-          <MessageFeed messages={this.state.messages.messages} />
+          <MessageFeed messages={this.state.messages} />
         </main>
-        <Chatbar user={this.state.messages.currentUser} />
+        <Chatbar user={this.state.currentUser} newMessage={this.addNewMessage}/>
         </div>
       );
     }
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 
+// function NewMessageForm({ addNewMessage }) {
+//     const onSubmit = evt => {
+//         evt.preventDefault();
+//         const newMessage = evt.target.elements.message;
+//         console.log('newMessage, ', newMessage);
+
+//     }
+// }
 
 class Chatbar extends Component {
-    render() {
-      return (
-        <footer className="chatbar">
-          <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.user.name} />
-          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-        </footer>
-      )
+  render() {
+    const onSubmit = evt => {
+      evt.preventDefault();
+      if (evt.keyCode === 13 && evt.target.name === 'message') {
+        const newMessage = evt.target.value;
+        const newMessageUser = evt.currentTarget.elements.user.value;
+        this.props.newMessage(newMessageUser, newMessage);
+        evt.target.value = '';
+      }
     }
+
+    return (
+      <footer>
+        <form className="chatbar" onKeyUp={onSubmit}>
+          <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.user.name} name="user"/>
+          <input name="message" className="chatbar-message" placeholder="Type a message and hit ENTER"/>
+        </form>
+      </footer>
+    )
   }
+}
 
 export default Chatbar
