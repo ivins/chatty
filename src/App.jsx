4e538888e18aaa93aaa58fc1ce3ebc24d5bf6301
silentscribe <seelive.i@gmail.com>
2019-02-12 04:51:25 +0000

    basic components set up and rendering properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
+import Chatbar from './Chatbar.jsx';
+import MessageFeed from './MessageList.jsx';
+import Message from './Message.jsx';
+import messages from './MessageDB.json'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+      <Navbar />
+      <main className="messages">
+        <Message />
+        <MessageFeed />
+      </main>
+      <Chatbar />
+      </div>
     );
   }
 }
